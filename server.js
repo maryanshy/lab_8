715adbfba8032262138e5b7c2f84eebc92466a8a
maryanshy <54924764+maryanshy@users.noreply.github.com>
2019-12-01 21:14:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const fetch = require('node-fetch');
 
 const app = express();
-const port = 5000;
+//const port = 5000;
+app.set('port', ((process.env.PORT || 5000));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
